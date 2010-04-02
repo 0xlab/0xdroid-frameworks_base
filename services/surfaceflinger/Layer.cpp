c17583a81f0a264e0cf13571e230222c741b683c
Kan-Ru Chen <kanru@0xlab.org>
2010-04-02 17:18:16 +0800

    Disable dithering by default for the sake of performance.
    This is a workaround for the performance drop on Eclair. Enabling
    dithering prevents the fast path in software renderer implementation
    thus the performance drops drastically.
    
    Disabling dithering has small impact on the rendering result, but the
    performance boosts a lot.

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -174,9 +174,12 @@ status_t Layer::setBuffers( uint32_t w, uint32_t h,
     mNeedsBlending = (info.h_alpha - info.l_alpha) > 0;
 
     // we use the red index
+#if 0
     int displayRedSize = displayInfo.getSize(PixelFormatInfo::INDEX_RED);
     int layerRedsize = info.getSize(PixelFormatInfo::INDEX_RED);
     mNeedsDithering = layerRedsize > displayRedSize;
+#endif
+    mNeedsDithering = false;
 
     mSurface = new SurfaceLayer(mFlinger, this);
     return NO_ERROR;
